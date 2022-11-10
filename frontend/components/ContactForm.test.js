a99a12c758303ae1f3193d9068ce1f8ffc4bc0fa
canasmh <canasmh@yahoo.com>
2022-11-10 00:39:06 -0500

    Check lastname error message displays if last name is left empty

--- frontend/components/ContactForm.test.js
+++ frontend/components/ContactForm.test.js
@@ -69,7 +69,15 @@ test('renders "email must be a valid email address" if an invalid email is enter
 });
 
 test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
+    render(<ContactForm />);
 
+    const submitBtn = screen.getByRole("button");
+    expect(submitBtn).toBeInTheDocument();
+
+    await waitFor(() => userEvent.click(submitBtn));
+    await waitFor(() => screen.getAllByText(/lastName is a required field/i));
+
+    expect(screen.getByText(/lastName is a required field/i)).toBeInTheDocument();
 });
 
 test('renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.', async () => {
